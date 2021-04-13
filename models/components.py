7edd060d696dc8ae0fbc71e33e6e56db5701fd25
LinfordDu <31502154+LinfordDu@users.noreply.github.com>
2021-04-14 01:52:50 +0800

    Update components.py

--- models/components.py
+++ models/components.py
@@ -98,18 +98,20 @@ def init_weights(m):
         nn.init.xavier_uniform_(m.weight)
         m.bias.data.zero_()
 
+
 class SelfAttention(nn.Module):
     """SelfAttention
     --------------------
     replica of https://github.com/heykeetae/Self-Attention-GAN
     """
-    def __init__(self,in_dim):
+
+    def __init__(self, in_dim):
         super(SelfAttention, self).__init__()
         self.chanel_in = in_dim
 
-        self.query_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim//8 , kernel_size= 1)
-        self.key_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim//8 , kernel_size= 1)
-        self.value_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim , kernel_size= 1)
+        self.query_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1)
+        self.key_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1)
+        self.value_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim, kernel_size=1)
         self.gamma = nn.Parameter(torch.zeros(1))
         self.softmax = nn.Softmax(dim=-1)
         init_weights(self.query_conv)
@@ -117,15 +119,15 @@ class SelfAttention(nn.Module):
         init_weights(self.value_conv)
 
     def forward(self, x):
-        m_batchsize, C, width, height = x.size()
-        proj_query = self.query_conv(x).view(m_batchsize, -1, width * height).permute(0, 2, 1)  # B X CX(N)
-        proj_key = self.key_conv(x).view(m_batchsize, -1, width * height)  # B X C x (*W*H)
-        energy = torch.bmm(proj_query, proj_key)  # transpose check
-        attention = self.softmax(energy)  # BX (N) X (N)
-        proj_value = self.value_conv(x).view(m_batchsize, -1, width * height)  # B X C X N
-
-        out = torch.bmm(proj_value, attention.permute(0, 2, 1))
-        out = out.view(m_batchsize, C, width, height)
+        m_batchsize, c, width, height = x.size()
+        proj_query = self.query_conv(x).view(m_batchsize, -1, width * height).permute(0, 2, 1)  # B * (c//8) * (W * H)
+        proj_key = self.key_conv(x).view(m_batchsize, -1, width * height)  # B * (c//8) * (W * H)
+        energy = torch.bmm(proj_query, proj_key)  # B * (W * H) * (W * H)
+        attention = self.softmax(energy)  # B  * (N) * (N)
+        proj_value = self.value_conv(x).view(m_batchsize, -1, width * height)  # B * c * (W * H)
+        # out = torch.bmm(proj_value, attention)  # B * c * (W * H)
+        out = torch.bmm(proj_value, attention.permute(0, 2, 1))  # B * c * (W * H)
+        out = out.view(m_batchsize, c, width, height)  # B * c * W * H
 
         out = self.gamma * out + x
         return out
